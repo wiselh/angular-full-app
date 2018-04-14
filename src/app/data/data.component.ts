df678b88343b7f37d249589a2acc5e591ebb48bd
wiselh <wise.self.2@gmail.com>
2018-04-14 13:56:52 +0100

    Post Request Data

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -9,13 +10,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataComponent implements OnInit {
 
-  posts : object;
+  posts: object;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(posts =>{
+    // get json reuest
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(posts => {
       this.posts = posts;
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    this.http.post('https://jsonplaceholder.typicode.com/posts', {
+    userId: Math.random(),
+    id: Math.random(),
+    title: form.value.title,
+    body: form.value.body
+    }).subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log('Error');
     });
   }
 
